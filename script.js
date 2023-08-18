07d58235722e029e6ec18f9daf41a17314b73049
Jongwan93 <jwhur1993@gmail.com>
2023-08-18 19:10:56 -0400

    add1

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const clicks = document.querySelectorAll(".click");
+const category = document.querySelectorAll(".click");
 const sections = document.querySelectorAll("section");
-const firstTop = sections[0].offsetTop;
-const secondTop = sections[1].offsetTop;
-const thirdTop = sections[2].offsetTop;
-const fourthTop = sections[3].offsetTop;
-const fifthTop = sections[4].offsetTop;
-const sixthTop = sections[5].offsetTop;
+const section1 = sections[0].offsetTop;
+const section2 = sections[1].offsetTop;
+const section3 = sections[2].offsetTop;
+const section4 = sections[3].offsetTop;
+const section5 = sections[4].offsetTop;
+const section6 = sections[5].offsetTop;
+//works well for 90% viewpoint in Chrome
+category[0].onclick = function () {
+  window.scroll({ top: section1, behavior: "smooth" });
+};
+category[1].onclick = function () {
+  window.scroll({ top: section2, behavior: "smooth" });
+};
+category[2].onclick = function () {
+  window.scroll({ top: section3, behavior: "smooth" });
+};
+category[3].onclick = function () {
+  window.scroll({ top: section4, behavior: "smooth" });
+};
+category[4].onclick = function () {
+  window.scroll({ top: section5, behavior: "smooth" });
+};
+category[5].onclick = function () {
+  window.scroll({ top: section6, behavior: "smooth" });
+};
 
-clicks[0].onclick = function () {
-  window.scroll({ top: firstTop, behavior: "smooth" });
-};
-clicks[1].onclick = function () {
-  window.scroll({ top: secondTop, behavior: "smooth" });
-};
-clicks[2].onclick = function () {
-  window.scroll({ top: thirdTop, behavior: "smooth" });
-};
-clicks[3].onclick = function () {
-  window.scroll({ top: fourthTop, behavior: "smooth" });
-};
-clicks[4].onclick = function () {
-  window.scroll({ top: fifthTop, behavior: "smooth" });
-};
-clicks[5].onclick = function () {
-  window.scroll({ top: sixthTop, behavior: "smooth" });
-};
-
-const topicRadio = document.querySelectorAll('input[name="topic"]');
+const radioQ = document.querySelectorAll('input[name="topic"]');
 const hourlyRateContainer = document.getElementById("hourlyRateContainer");
 
-topicRadio.forEach((radio) => {
+radioQ.forEach((radio) => {
   radio.addEventListener("change", () => {
     if (radio.value === "Hiring") {
       hourlyRateContainer.style.display = "block";
